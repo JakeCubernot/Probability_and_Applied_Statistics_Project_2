c58b5a44dccf14a7c011154f3de98bf83822c964
Jake Cubernot <jakecubernot@gmail.com>
2024-03-25 11:37:13 -0400

    Added the completed plotter work

--- Probability_and_Applied_Statistics_Project_2/src/plotter/Plotter.java
+++ Probability_and_Applied_Statistics_Project_2/src/plotter/Plotter.java
@@ -1,23 +1,55 @@
 package plotter;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class Plotter {
 
-    public String[][] graphSineFunction() {
-        String[][] sineFunctionResult = new String[200 + 1][2];
-        System.out.println(sineFunctionResult.length);
-        for (int i = -100; i <= 100; i++) {
-			sineFunctionResult[i + 100][0] = Integer.toString(i);
-			sineFunctionResult[i + 100][1] = Double.toString(getYOfSineFunction(i));
+    private String[][] graphFunction(double minValue, double maxValue, double incrementValue) {
+		int lengthOfX = (int) Math.ceil((maxValue - minValue) / incrementValue) + 1;
+        String[][] sineFunctionResult = new String[lengthOfX][2];
+        for (int i = 0; i < lengthOfX; i++) {
+			double valueOfX = minValue + i * incrementValue;
+			sineFunctionResult[i][0] = Double.toString(valueOfX);
+			sineFunctionResult[i][1] = Double.toString(getYOfFunction(valueOfX));
         }
         return sineFunctionResult;
     }
 
-	private double getYOfSineFunction(int x) {
-		return Math.sin(x);
+	private double getYOfFunction(double x) {
+		return Math.sin(x / 2);
+	}
+
+	private String[][] saltGraph(String[][] userGraph) {
+		Random rng = new Random();
+		for (int i = 0; i < userGraph.length; i++) {
+			double oldYValue = Double.parseDouble(userGraph[i][1]);
+			double newYValue = oldYValue + rng.nextDouble(-1, 1);
+			userGraph[i][1] = String.valueOf(newYValue);
+		}
+		return userGraph;
+	}
+
+	private String[][] smoothGraph(String[][] userGraph) {
+		for (int i = 1; i < userGraph.length - 1; i++) {
+			double previousYValue = Double.parseDouble(userGraph[i - 1][1]);
+			double currentYValue = Double.parseDouble(userGraph[i][1]);
+			double nextYValue = Double.parseDouble(userGraph[i + 1][1]);
+			userGraph[i][1] = String.valueOf(getYAverage(previousYValue, currentYValue, nextYValue));
+		}
+		return userGraph;
+	}
+
+	private double getYAverage(double previousYValue, double currentYValue, double nextYValue) {
+		return (previousYValue + currentYValue + nextYValue) / 3;
 	}
 
     // Method from https://springhow.com/java-write-csv/.
@@ -41,8 +73,36 @@ public class Plotter {
     	fileWriter.close();
     }
 
-    public void run() throws IOException {
-        String[][] userSineFunction = graphSineFunction();
-		writeCSVFile(userSineFunction, "User Sine Function.csv");
+	// Method inspired from https://www.baeldung.com/java-csv-file-array.
+	private String[][] openCSVFile(String fileName) throws FileNotFoundException, IOException {
+		List<List<String>> records = new ArrayList<>();
+		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				String[] values = line.split(",");
+				records.add(Arrays.asList(values));
+			}
+		}
+		String[][] stringArray = new String[records.size()][];
+		for (int i = 0; i < records.size(); i++) {
+			List<String> innerList = records.get(i);
+			stringArray[i] = innerList.toArray(new String[0]);
+		}
+		return stringArray;
+	}
+
+    public void runFunctionPlotter(double minValue, double maxValue, double incrementValue) throws IOException {
+        String[][] userSineFunction = graphFunction(minValue, maxValue, incrementValue);
+		writeCSVFile(userSineFunction, "User Function.csv");
     }
+
+	public void runSalter(String fileName) throws FileNotFoundException, IOException {
+		String[][] userSineFunction = saltGraph(openCSVFile(fileName));
+		writeCSVFile(userSineFunction, "User Function with Salting.csv");
+	}
+
+	public void runSmoother(String fileName, String newFileName) throws IOException {
+		String[][] userSineFunction = smoothGraph(openCSVFile(fileName));
+		writeCSVFile(userSineFunction, newFileName);
+	}
 }
--- Probability_and_Applied_Statistics_Project_2/src/plotter/TestPlotter.java
+++ Probability_and_Applied_Statistics_Project_2/src/plotter/TestPlotter.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 public class TestPlotter {
     public static void main(String[] args) throws IOException {
         Plotter testPlotter = new Plotter();
-        testPlotter.run();
+        testPlotter.runFunctionPlotter(-25, 25, 0.1);
+        testPlotter.runSalter("User Function.csv");
+        testPlotter.runSmoother("User Function with Salting.csv", "User Function with 1 Smoother.csv");
+        testPlotter.runSmoother("User Function with 1 Smoother.csv", "User Function with 2 Smoothers.csv");
+        testPlotter.runSmoother("User Function with 2 Smoothers.csv", "User Function with 3 Smoothers.csv");
     }
 }
