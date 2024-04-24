c0979353044dc6af8d09b3cdc71bbd130b6764be
Jake Cubernot <jakecubernot@gmail.com>
2024-04-24 17:41:57 -0400

    Completed research report

--- Probability_and_Applied_Statistics_Project_2/src/stockBot/StockBot.java
+++ Probability_and_Applied_Statistics_Project_2/src/stockBot/StockBot.java
@@ -17,36 +17,32 @@ import org.jfree.data.xy.XYSeriesCollection;
 
 import regularPlotter.RegularPlotter;
 
-/**
- * Stock bot using JFreeChart
- * 
- * Sources: 
- * https://stackoverflow.com/a/12318835
- */
 public class StockBot extends RegularPlotter {
     public static void main(String[] args) throws FileNotFoundException, IOException {
         XYSeries stockFunction = new XYSeries("Microsoft Corp, MSFT, Stock (1Y)"); 
+        XYSeries rsiFunction = new XYSeries("RSI Calculation"); 
         
         StockBot plotter = new StockBot();
 
         String[][] graphValues = plotter.openCSVFile("F:/Probability_and_Applied_Statistics_Project_2/Probability_and_Applied_Statistics_Project_2/src/stockBot/MSFT.csv");
         for (int i = 0; i < graphValues.length; i++) {
-            stockFunction.add(i, Double.parseDouble(graphValues[i][1]));
-        }
+            stockFunction.add(i, Double.parseDouble(graphValues[i][2])); 
+        } 
               
-        XYSeriesCollection dataset = new XYSeriesCollection(stockFunction);
+        XYSeriesCollection dataset = new XYSeriesCollection(stockFunction); 
+        dataset.addSeries(rsiFunction); 
 
         JFreeChart chart = ChartFactory.createXYLineChart("Microsoft Corp, MSFT, Stock (1Y)", null, null, 
         dataset, PlotOrientation.VERTICAL, true, true, true);
 
-        XYPlot plot = (XYPlot) chart.getPlot();
-        plot.getRangeAxis().setRange(250, 450);
-        plot.getDomainAxis().setRange(0, 53);
-        plot.getDomainAxis().setTickLabelsVisible(false);
+        XYPlot plot = (XYPlot) chart.getPlot(); 
+        plot.getRangeAxis().setRange(250, 450); 
+        plot.getDomainAxis().setRange(0, 53); 
+        plot.getDomainAxis().setTickLabelsVisible(false); 
 
-        ChartPanel chartpanel = new ChartPanel(chart);
-        chartpanel.setDomainZoomable(false);
-        chartpanel.setRangeZoomable(false);
+        ChartPanel chartpanel = new ChartPanel(chart); 
+        chartpanel.setDomainZoomable(false); 
+        chartpanel.setRangeZoomable(false); 
 
         JPanel jPanel4 = new JPanel();
         jPanel4.setLayout(new BorderLayout());
@@ -56,9 +52,5 @@ public class StockBot extends RegularPlotter {
         frame.add(jPanel4);
         frame.pack();
         frame.setVisible(true);
-    }
-
-    private double calculateRSI() {
-        
     }
 }
