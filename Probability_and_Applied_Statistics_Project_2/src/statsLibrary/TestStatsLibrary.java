2f6925078d97a11786e26c4f2881cd3f1e3389b9
Jake Cubernot <jakecubernot@gmail.com>
2024-04-26 21:12:34 -0400

    Added the stats library documentation

--- Probability_and_Applied_Statistics_Project_2/src/statsLibrary/TestStatsLibrary.java
+++ Probability_and_Applied_Statistics_Project_2/src/statsLibrary/TestStatsLibrary.java
@@ -1,5 +1,16 @@
 package statsLibrary;
 
 public class TestStatsLibrary {
-    
+    public static void main(String[] args) {
+        StatsLibrary test = new StatsLibrary();
+
+        double testResult = test.findPoissonDistribution(1, 3);
+        System.out.println("Poisson Distribution where x=1 and lambda=3: " + (testResult * 100) + "%"); 
+
+        testResult = test.findChebyshevsTheorem(2);
+        System.out.println("\nChebyshev's Theorem where k=2: " + (testResult * 100) + "%"); 
+
+        testResult = test.findUniformDistribution(2, 5); 
+        System.out.println("\nUniform Distribution where a=2 and b=5: " + (testResult * 100) + "%"); 
+    }
 }
